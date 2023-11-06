5ec1094819dfd1ee868adb1194c8e1b19efdfde8
Christian Bound <automaticbound@gmail.com>
2023-11-06 17:47:47 +0900

    exercise1.4 complete

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,17 +1,20 @@
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   const Header = (props) => {
     return (
@@ -32,6 +35,7 @@ const App = () => {
   };
 
   const Content = (props) => {
+    console.log(props);
     return (
       <div>
         <Part part={props.parts[0]} />
@@ -57,8 +61,8 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content parts={[part1, part2, part3]} />
-      <Total parts={[part1, part2, part3]} />
+      <Content parts={parts} />
+      <Total parts={parts} />
 
       {/* <Content content={content} />
       <Total  /> */}
