f2bd8b93a61cc4b161f2b831b3d66e77274a8299
Christian Bound <automaticbound@gmail.com>
2023-11-09 16:35:07 +0900

    exercise2.5 complete

--- part2/courseinfoREDUX/src/App.jsx
+++ part2/courseinfoREDUX/src/App.jsx
@@ -1,32 +1,4 @@
-const Course = ({ courses }) => {
-  return courses.map((course) => (
-    <div key={course.id}>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  ));
-};
-
-const Header = ({ course }) => <h1>{course}</h1>;
-
-const Part = ({ part }) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-);
-
-const Content = ({ parts }) => {
-  return parts.map((part) => <Part part={part} key={part.id} />);
-};
-
-const Total = ({ parts }) => {
-  return (
-    <h3>
-      Number of exercises {parts.reduce((acc, part) => acc + part.exercises, 0)}
-    </h3>
-  );
-};
+import Course from "./components/Course";
 
 const App = () => {
   const courses = [
