012606fe49a0a3a00dc42050a589e526975bfa61
Christian Bound <automaticbound@gmail.com>
2024-01-29 17:32:20 +0900

    exercises 4.1 & 4.2 completed

--- part4/index.js
+++ part4/index.js
@@ -1,1 +1,6 @@
-console.log('Hello World')
+const app = require("./app");
+const config = require("./utils/config");
+
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`);
+});
