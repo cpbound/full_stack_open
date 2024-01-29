54e27777257ecb5ec3ad86cf31713e055d002bcb
Christian Bound <automaticbound@gmail.com>
2024-01-29 19:02:11 +0900

    exercise 4.4 completed

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -6,7 +6,13 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
 
+const favouriteBlog = (blogs) => {
+  const sorted = blogs.sort((a, b) => b.likes - a.likes);
+  return sorted[0];
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favouriteBlog,
 };
