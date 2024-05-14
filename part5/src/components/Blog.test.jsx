ec659cee2a4ee8326c1b28185605c2f31a35f3b2
Christian Bound <automaticbound@gmail.com>
2024-05-14 17:42:40 +0900

    exercise5.14 completed

--- part5/src/components/Blog.test.jsx
+++ part5/src/components/Blog.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from '@testing-library/react'
+import { userEvent } from '@testing-library/user-event'
 import Blog from './Blog'
-import { expect } from 'vitest'
+import { beforeEach, describe, expect } from 'vitest'
 
 test('renders content', () => {
   const blog = {
@@ -26,5 +27,31 @@ test('renders content', () => {
   expect(div).toHaveTextContent(
     'Component testing is done with react-testing-library'
   )
-  expect(div).toHaveTextContent('Gazza Chuff')
+  expect(div).toHaveTextContent('Author: Gazza Chuff')
+})
+
+test('renders additional content after clicking view button', () => {
+  const blog = {
+    title: 'Component testing is done with react-testing-library',
+    author: 'Gazza Chuff',
+    url: 'www.test.test',
+    likes: '0',
+    user: {
+      username: 'GCHuff',
+      id: 'test',
+    },
+  }
+
+  const user = {
+    username: 'test',
+    name: 'Kenny Loggedin',
+    id: 'usertest',
+  }
+
+  const { container } = render(<Blog blog={blog} user={user} />)
+
+  const div = container.querySelector('.hidden')
+
+  expect(div).toHaveTextContent('www.test.test')
+  expect(div).toHaveTextContent('0 ♥️')
 })
