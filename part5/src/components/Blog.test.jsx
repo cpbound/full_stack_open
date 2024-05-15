23e1c7f3513e74b58e8a7a70380d16d5ab7bce89
Christian Bound <automaticbound@gmail.com>
2024-05-15 17:32:57 +0900

    exercise5.15 completed

--- part5/src/components/Blog.test.jsx
+++ part5/src/components/Blog.test.jsx
@@ -55,3 +55,33 @@ test('renders additional content after clicking view button', () => {
   expect(div).toHaveTextContent('www.test.test')
   expect(div).toHaveTextContent('0 ♥️')
 })
+
+test('clicking the like button twice increases the number of likes accordingly', async () => {
+  const blog = {
+    title: 'Component testing is done with react-testing-library',
+    author: 'Gazza Chuff',
+    url: 'www.test.test',
+    likes: '0',
+    user: {
+      username: 'GCHuff',
+      id: 'test',
+    },
+  }
+
+  const user = {
+    username: 'test',
+    name: 'Kenny Loggedin',
+    id: 'usertest',
+  }
+
+  const mockHandler = vi.fn()
+
+  render(<Blog blog={blog} user={user} updateLikes={mockHandler} />)
+
+  const interact = userEvent.setup()
+  const button = screen.getByText('Like')
+  await interact.click(button)
+  await interact.click(button)
+
+  expect(mockHandler.mock.calls).toHaveLength(2)
+})
