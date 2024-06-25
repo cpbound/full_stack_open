491b695fffecfc1ec77c130bb8dcdb3df9e6daab
Christian Bound <automaticbound@gmail.com>
2024-06-25 17:18:56 +0900

    exercise6.10 complete

--- part6/redux-anecdotes/src/main.jsx
+++ part6/redux-anecdotes/src/main.jsx
@@ -1,16 +1,23 @@
 import ReactDOM from 'react-dom/client'
-import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
+import { configureStore } from '@reduxjs/toolkit'
 import App from './App'
 import anecdoteReducer from './reducers/anecdoteReducer'
 import filterReducer from './reducers/filterReducer'
 
-const reducer = combineReducers({
-  anecdotes: anecdoteReducer,
-  filters: filterReducer,
+const store = configureStore({
+  reducer: {
+    anecdotes: anecdoteReducer,
+    filters: filterReducer,
+  },
 })
 
-const store = createStore(reducer)
+// const reducer = combineReducers({
+//   anecdotes: anecdoteReducer,
+//   filters: filterReducer,
+// })
+
+// const store = createStore(reducer)
 
 console.log(store.getState())
 
--- part6/redux-anecdotes/src/reducers/filterReducer.js
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,18 +1,33 @@
-const filterReducer = (state = null, action) => {
-  console.log(action.type);
-  switch (action.type) {
-    case "SET_FILTER":
+import { createSlice } from "@reduxjs/toolkit";
+
+
+
+// const filterReducer = (state = null, action) => {
+//   console.log(action.type);
+//   switch (action.type) {
+//     case "SET_FILTER":
+//       return action.payload;
+//     default:
+//       return state;
+//   }
+// };
+
+// export const filterChange = (filter) => {
+//   return {
+//     type: "SET_FILTER",
+//     payload: filter,
+//   };
+// };
+
+const filterSlice = createSlice({
+  name: "filter",
+  initialState: null,
+  reducers: {
+    filterChange: (state, action) => {
       return action.payload;
-    default:
-      return state;
-  }
-};
+    }
+  },
+});
 
-export const filterChange = (filter) => {
-  return {
-    type: "SET_FILTER",
-    payload: filter,
-  };
-};
-
-export default filterReducer;
+export const { filterChange } = filterSlice.actions
+export default filterSlice.reducer
