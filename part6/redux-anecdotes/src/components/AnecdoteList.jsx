985376eb543b1d0b7fcd3e4bdb6b8d0b3877a5a1
Christian Bound <automaticbound@gmail.com>
2025-04-09 15:46:28 +0900

    exercise6.14 finally finished

--- part6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ part6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,38 +1,44 @@
 import { useSelector, useDispatch } from "react-redux";
 import { addVote } from "../reducers/anecdoteReducer";
-import { setNotification, clearNotification } from "../reducers/notificationReducer";
+import {
+  setNotification,
+  clearNotification,
+} from "../reducers/notificationReducer";
 
 const AnecdoteList = () => {
   const anecdotes = useSelector((state) => state.anecdotes);
   const filter = useSelector((state) => state.filter);
   const dispatch = useDispatch();
 
+  console.log("anecdotes", anecdotes);
   const filteredAnecdotes = anecdotes.filter((anecdote) =>
     anecdote.content.toLowerCase().includes(filter.toLowerCase())
   );
 
   const upVote = (id, content) => {
     dispatch(addVote(id));
-    dispatch(setNotification(`${content} was up voted!`))
+    dispatch(setNotification(`${content} was up voted!`));
     setTimeout(() => {
-      dispatch(clearNotification())
-    }, 5000)
-  }
+      dispatch(clearNotification());
+    }, 5000);
+  };
 
   return (
     <div>
       {filteredAnecdotes.map((anecdote) => (
-        <div key={anecdote.id}>
-          <div>{anecdote.content}</div>
-          <div>
-            <strong>has</strong> {anecdote.votes} votes.
+          <div key={anecdote.id}>
+            <div>{anecdote.content}</div>
             <div>
-              <button onClick={() => upVote(anecdote.id, anecdote.content)}>Vote</button>
+              <strong>has</strong> {anecdote.votes} votes.
+              <div>
+                <button onClick={() => upVote(anecdote.id, anecdote.content)}>
+                  Vote
+                </button>
+              </div>
             </div>
+            <br />
           </div>
-          <br />
-        </div>
-      ))}
+        ))}
     </div>
   );
 };
--- part6/redux-anecdotes/src/store.js
+++ part6/redux-anecdotes/src/store.js
@@ -1,6 +1,7 @@
-import anecdoteReducer from './reducers/anecdoteReducer'
+import anecdoteReducer, { setAnecdotes } from './reducers/anecdoteReducer'
 import filterReducer from './reducers/filterReducer'
 import notificationReducer from './reducers/notificationReducer'
+import anecdoteService from './services/anecdotes'
 import { configureStore } from '@reduxjs/toolkit'
 
 const store = configureStore({
@@ -10,5 +11,9 @@ const store = configureStore({
     notification: notificationReducer
   }
 })
+
+anecdoteService.getAll().then(anecdotes =>
+store.dispatch(setAnecdotes(anecdotes))
+)
 
 export default store
