65d549be0250bc2d32a9f17d96d2b9dcf0085f25
Christian Bound <automaticbound@gmail.com>
2025-05-12 17:27:40 +0900

    exercise 8.7 completed

--- part8/graphQL-library/library-backend.js
+++ part8/graphQL-library/library-backend.js
@@ -127,7 +127,11 @@ const typeDefs = `
       author: String!
       published: Int!
       genres: [String!]!
-    ): Book!
+    ): Book
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
     `;
 
@@ -168,6 +172,17 @@ const resolvers = {
       const book = { ...args, id: uuid() };
       books = books.concat(book);
       return book;
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find((a) => a.name === args.name);
+      if (!author) {
+        return null;
+      }
+
+      const updatedAuthor = { ...author, born: args.setBornTo };
+      authors = authors.map((a) => (a.name === args.name ? updatedAuthor : a));
+
+      return updatedAuthor;
     },
   },
 };
