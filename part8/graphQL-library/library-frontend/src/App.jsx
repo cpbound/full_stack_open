3b2f1ad0fe84b57b6690dd209c6795feb3c95d42
Christian Bound <automaticbound@gmail.com>
2025-05-19 17:16:32 +0900

    exercises 8.17 & 8.18 completed

--- part8/graphQL-library/library-frontend/src/App.jsx
+++ part8/graphQL-library/library-frontend/src/App.jsx
@@ -1,25 +1,89 @@
+import { useApolloClient, useQuery } from "@apollo/client";
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Authors from "./components/Authors";
+import LoginForm from "./components/LoginForm";
 import Books from "./components/Books";
 import NewBook from "./components/NewBook";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Notification from "./components/Notification";
 
 const App = () => {
+  const [token, setToken] = useState(null);
+  const [error, setError] = useState(null);
+  const client = useApolloClient();
+
+  useEffect(() => {
+    const storedToken = localStorage.getItem("library-user-token");
+    if (storedToken) {
+      setToken(storedToken);
+    }
+  }, []);
+
+  const logout = () => {
+    setToken(null);
+    localStorage.clear();
+    client.resetStore();
+  };
+
+  console.log(localStorage, client)
+
   return (
     <Router>
+      <Notification notification={error} />
       <div>
-        <div>
-          <nav>
-            <button>
-              <Link to="/authors">Authors</Link>
-            </button>
-            <button>
-              <Link to="/books">Books</Link>
-            </button>
-            <button>
-              <Link to="/add">Add book</Link>
-            </button>
-          </nav>
-        </div>
+        {!token ? (
+          <>
+            <LoginForm setToken={setToken} setError={setError} />
+            <nav>
+              <button>
+                <Link
+                  style={{ textDecoration: "none", color: "inherit" }}
+                  to="/authors"
+                >
+                  Authors
+                </Link>
+              </button>
+              <button>
+                <Link
+                  style={{ textDecoration: "none", color: "inherit" }}
+                  to="/books"
+                >
+                  Books
+                </Link>
+              </button>
+            </nav>
+          </>
+        ) : (
+          <div>
+            <button onClick={() => logout()}>Logout</button>
+            <nav>
+              <button>
+                <Link
+                  style={{ textDecoration: "none", color: "inherit" }}
+                  to="/authors"
+                >
+                  Authors
+                </Link>
+              </button>
+              <button>
+                <Link
+                  style={{ textDecoration: "none", color: "inherit" }}
+                  to="/books"
+                >
+                  Books
+                </Link>
+              </button>
+              <button>
+                <Link
+                  style={{ textDecoration: "none", color: "inherit" }}
+                  to="/add"
+                >
+                  Add book
+                </Link>
+              </button>
+            </nav>
+          </div>
+        )}
         <Routes>
           <Route path="/authors" element={<Authors />} />
           <Route path="/books" element={<Books />} />
---
+++ part8/graphQL-library/library-frontend/src/components/Notification.jsx
@@ -0,0 +1,17 @@
+const Notification = ({ notification }) => {
+  const style = {
+    border: "solid",
+    padding: 8,
+    borderRadius: 8,
+    margin: 24,
+    borderWidth: 1,
+  };
+
+  if (!notification) {
+    return null;
+  }
+
+  return <div style={style}>{notification}</div>;
+};
+
+export default Notification;
