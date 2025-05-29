6733f7b890382bb28d1ca50d2837edd5f71edc96
Christian Bound <automaticbound@gmail.com>
2025-05-29 16:32:37 +0900

    exercise 9.5 completed

--- part9/ts-exercises/bmiCalculator.ts
+++ part9/ts-exercises/bmiCalculator.ts
@@ -22,7 +22,7 @@ const parseArguments = (args: string[]): BmiValues => {
   }
 };
 
-const calculateBmi = (height: number, weight: number): string => {
+export const calculateBmi = (height: number, weight: number): string => {
   const heightFix = height / 100;
   const bmi = weight / (heightFix * heightFix);
   if (bmi < 18.5) {
@@ -34,13 +34,15 @@ const calculateBmi = (height: number, weight: number): string => {
   }
 };
 
-try {
-  const { height, weight } = parseArguments(process.argv);
-  console.log(calculateBmi(height, weight));
-} catch (error: unknown) {
-  let errorMessage = "Something bad happened.";
-  if (error instanceof Error) {
-    errorMessage += " Error: " + error.message;
+if (require.main === module) {
+  try {
+    const { height, weight } = parseArguments(process.argv);
+    console.log(calculateBmi(height, weight));
+  } catch (error: unknown) {
+    let errorMessage = "Something bad happened.";
+    if (error instanceof Error) {
+      errorMessage += " Error: " + error.message;
+    }
+    console.log(errorMessage);
   }
-  console.log(errorMessage);
 }
--- part9/ts-exercises/index.ts
+++ part9/ts-exercises/index.ts
@@ -1,8 +1,21 @@
-import express from 'express';
+import express from "express";
+import { calculateBmi } from "./bmiCalculator";
 const app = express();
 
-app.get('/hello', (_req, res) => {
-  res.send('Hello Full Stack!');
+app.get("/hello", (_req, res) => {
+  res.send("Hello Full Stack!");
+});
+
+app.get("/bmi", (req, res) => {
+  const height = Number(req.query.height);
+  const weight = Number(req.query.weight);
+
+  if (!height || !weight || isNaN(height) || isNaN(weight)) {
+    return res.status(400).json({ error: "malformatted parameters" });
+  }
+
+  const bmi = calculateBmi(height, weight);
+  return res.json({ weight, height, bmi });
 });
 
 const PORT = 3003;
